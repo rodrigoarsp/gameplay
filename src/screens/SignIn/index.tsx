a199bc139da9717ed2ff53442c4b1ecb963578cb
Rodrigo Souza <rodrigoarsp@gmail.com>
2021-06-21 21:25:04 -0300

    singnin

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,18 +3,45 @@ import {
   View,
   Text,
   Image,
+  StatusBar
 } from "react-native";
 
-import IllustrationImg from '../../assets/Illustration.png';
+import { ButtonIcon } from "../../components/ButtonIcon";
+import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
 export function SignIn(){
-  const [text, setText] = useState('');
-
   return(
     <View style={styles.container}>
       
-      <Image source={IllustrationImg} />
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Image 
+        source={IllustrationImg} 
+        style={styles.image}
+        resizeMode='stretch'
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente {`\n`}
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon 
+          title="Entrar com Discord"
+          activeOpacity={0.7}
+        />
+      </View>
 
     </View>
   );
